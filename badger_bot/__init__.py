6aa9e1fe7c7586ad12a8968ddee4cb00d84b41d4
Lew Chewning <lew@lewchewning.com>
2022-08-23 16:11:43 +0000

    mysql work

--- badger_bot/__init__.py
+++ badger_bot/__init__.py
@@ -1,17 +1,30 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_mysqldb import MySQL
 from flask_login import LoginManager
 import json
 import urllib3
 
 app = Flask(__name__)
 
-with open('/etc/config.json') as config_file:
-  config = json.load(config_file)
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'flask'
 
-app.config['SECRET_KEY'] = config.get('SECRET_KEY')
-app.config['SQLALCHEMY_DATABASE_URI'] = config.get('SQLALCHEMY_DATABASE_URI')
-db = SQLAlchemy(app)
+mysql = MySQL(app)
+
+cursor = mysql.connection.cursor()
+ 
+#Executing SQL Statements
+cursor.execute(''' CREATE TABLE table_name(field1, field2...) ''')
+cursor.execute(''' INSERT INTO table_name VALUES(v1,v2...) ''')
+cursor.execute(''' DELETE FROM table_name WHERE condition ''')
+ 
+#Saving the Actions performed on the DB
+mysql.connection.commit()
+ 
+#Closing the cursor
+cursor.close()
 
 login_manager = LoginManager()
 login_manager.init_app(app)
