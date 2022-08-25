c058f7760a5edcc3804451b4f7699b2b9fe8d6a3
Lew Chewning <lew@lewchewning.com>
2022-08-25 16:14:58 +0000

    data work

--- badger_bot/__init__.py
+++ badger_bot/__init__.py
@@ -2,16 +2,19 @@ import sqlite3
 from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from dotenv import load_dotenv
 import pprint
 import json
 import urllib3
 import os
 
+load_dotenv()
 
 app = Flask(__name__)
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = ("mysql://"+ os.environ['MYSQL_USER'] + ":" + os.environ['MYSQL_PASSWORD']+ "@" + os.environ['MYSQL_HOST'] + ":3306/badger")
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql://"+ os.environ.get('MYSQL_USER','sqlite') + ":" + os.environ.get('MYSQL_PASSWORD',)+ "@" + os.environ.get('MYSQL_HOST',) + ":3306/badger"
+
 
 app.secret_key = os.urandom(24)
 
